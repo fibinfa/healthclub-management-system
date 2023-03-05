185877980d6a3e69264360c373674588815449d3
MeeraTresa <meeratresa@gmail.com>
2023-03-05 09:43:08 -0800

    Initial commit

---
+++ healthclub-backend/src/main/java/com/CMPE202/healthclub/HealthClubApplication.java
@@ -0,0 +1,14 @@
+package com.CMPE202.healthclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication()
+public class HealthClubApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HealthClubApplication.class, args);
+	}
+
+
+}
